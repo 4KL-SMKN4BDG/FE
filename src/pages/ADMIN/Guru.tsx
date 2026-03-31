66be1ca4a1950b1f6a6e8df25b1386828f49ae6f
wandradanurwenda-sketch <wandradanurwenda@gmail.com>
2026-04-01 01:20:37 +0700

    gacor

--- src/pages/ADMIN/Guru.tsx
+++ src/pages/ADMIN/Guru.tsx
@@ -17,12 +17,12 @@ import { useSearchParams } from "react-router-dom";
     const { user } = useAuthStore();
     const roleSearch = searchParams.get("role") === "teacher" ? "TEACHER" : "STUDENT"; 
     const role = user?.roles[0]?.code === "ADMIN" ? "ADMIN" : "STUDENT";
-    const payload = `paginate=true&limit=10&where=roles.some.code:${roleSearch}`;
+    const payload = `paginate=true&limit=30&where=roles.some.code:${roleSearch}`;
 
     useEffect(() => {
         showAll(payload);
     }, [])
-
+    
     const toggleTheme = () => {
     setTheme(prev => (prev === "lofi" ? "night" : "lofi"));
 };
